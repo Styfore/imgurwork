329f7a7cdf6e300eede1a6301aa7156ca47b760c
Thomas Pensec <thomas.pensec@insee.fr>
2017-10-20 09:58:01 +0200

    maj script greasemonkey

--- imgurwork_greasemonkey.js
+++ imgurwork_greasemonkey.js
@@ -5,16 +5,19 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-var domain = 'http://localhost/';
+var domain = 'http://imwork.styfore.ovh/';
 
 function creerLien(originalLink) {
   var splitByImgur = originalLink.split('imgur.com/');
   var retour = originalLink;
+  console.info(splitByImgur);
   if (splitByImgur.length == 2) {
     var splitByDot = splitByImgur[1].split('.');
     if (splitByDot.length > 0) {
+      console.info(splitByDot);
       var id = splitByDot[0];
-      if (id.split("/").length == 0){
+      console.info(id);
+      if (id.split("/").length == 1){
         id = "i/" + id;
       }
       
@@ -25,5 +28,17 @@ function creerLien(originalLink) {
 };
 
 $('.commentarea .usertext-body > div  a').hover(function () {
+  $(this).attr('href', creerLien($(this).attr('href')));
+});
+
+$('div.link div.entry a.title').hover(function () {
+  $(this).attr('href', creerLien($(this).attr('href')));
+  $(this).attr('data-href-url', creerLien($(this).attr('data-href-url')));
+  $(this).removeAttr("data-outbound-expiration");
+  $(this).removeAttr("data-outbound-url");
+  
+});
+
+$('#siteTable div.entry div.expando a').hover(function () {
   $(this).attr('href', creerLien($(this).attr('href')));
 });
